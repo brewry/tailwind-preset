d1735f23e5d28c9680ea2436ce5475dfcb252a7b
Chris Magnussen <chris@hirvi.no>
2020-11-26 10:04:15 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    100: '#FAFAFA',
+                    200: '#EAEAEA',
+                    300: '#999999',
+                    400: '#888888',
+                    500: '#666666',
+                    600: '#444444',
+                    700: '#333333',
+                    800: '#111111',
+                    900: '#000000',
+                },
+                error: {
+                    lighter: '#F7D4D6',
+                    light: '#FF1A1A',
+                    default: '#E00',
+                    dark: '#C50000',
+                },
+                success: {
+                    lighter: '#D3E5FF',
+                    light: '#3291FF',
+                    default: '#0070F3',
+                    dark: '#0761D1',
+                },
+                warning: {
+                    lighter: '#FFEFCF',
+                    light: '#F7B955',
+                    default: '#F5A623',
+                    dark: '#AB570A',
+                },
+                violet: {
+                    lighter: '#E3D7FC',
+                    light: '#8A63D2',
+                    default: '#7928CA',
+                    dark: '#4C2889',
+                },
+                cyan: {
+                    lighter: '#AAFFEC',
+                    light: '#79FFE1',
+                    default: '#50E3C2',
+                    dark: '#29BC9B',
+                },
+                highlight: {
+                    lighter: '#F81CE5',
+                    light: '#EB367F',
+                    default: '#FF0080',
+                    dark: '#FFF500',
+                },
+            },
+            boxShadow: {
+                smallest: '0px 4px 8px rgba(0,0,0,0.12)',
+                small: '0 5px 10px 0 rgba(0, 0, 0, 0.12)',
+                medium: '0 8px 30px rgba(0,0,0,0.12)',
+                large: '0 30px 60px rgba(0,0,0,0.12)',
+            },
+        },
+    },
+    variants: {},
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# @brewry/tailwind-preset
+
+This is a tailwindcss configuration that is shared across all [@brewry](https://github.com/brewry) projects.
+
+
+## Installation
+
+```shell
+$ npm i @brewry/tailwind-preset
+```
+
+## Usage
+
+You can use this preset by putting this in your project's `tailwind.config.js`: 
+
+```js
+module.exports = {
+  presets: [require('@brewry/tailwind-preset')],
+  theme: { ... }
+}
+```
